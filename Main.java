1547afc132d3cfd24e86b9815736a4a1a308dc47
Shubham Nahar <shunahar@publicisgroupe.net>
2024-07-08 09:49:16 +0530

    Linked List Basics

--- Main.java
+++ Main.java
@@ -13,10 +13,47 @@ public class Main {
 
         myLinkedList.printList();
 
-        System.out.println(myLinkedList.removeLast().value);
-        System.out.println(myLinkedList.removeLast().value);
-        System.out.println(myLinkedList.removeLast().value);
-        System.out.println(myLinkedList.removeLast().value);
-        System.out.println(myLinkedList.removeLast());
+        System.out.println("Prepend: \n");
+        // System.out.println(myLinkedList.removeLast().value);
+        // System.out.println(myLinkedList.removeLast().value);
+        // System.out.println(myLinkedList.removeLast().value);
+        // System.out.println(myLinkedList.removeLast().value);
+        // System.out.println(myLinkedList.removeLast());
+
+        myLinkedList.prepend(5);
+        myLinkedList.printList();
+
+        System.out.println("First Item Removed is: " + myLinkedList.removeFirst().value);
+
+        System.out.println("Get Element from Index 2: " + myLinkedList.getIndex(2).value);
+
+        System.out.println("Get Element from Index 4: " + myLinkedList.getIndex(4));
+
+        myLinkedList.setValue(2, 6);
+
+        myLinkedList.setValue(2, 6); //will return false and not execute
+
+        myLinkedList.printList();
+
+        myLinkedList.insert(4, 7);
+        myLinkedList.insert(2, 8);
+        myLinkedList.insert(0, 9);
+
+        System.out.println("After Inserting Value: \n ");
+        myLinkedList.printList();
+
+        System.out.println("Remove element at Index 3: " +myLinkedList.remove(3).value);
+        System.out.println("Remove element at Index 0: " +myLinkedList.remove(0).value);
+        System.out.println("remove last element: " +myLinkedList.remove(4).value);
+
+        System.out.println("After removing Value: \n ");
+        myLinkedList.printList();
+
+        System.out.println("remove last element: " +myLinkedList.remove(4));
+
+        myLinkedList.reverse();
+
+        System.out.println("List After reverse");
+        myLinkedList.printList();
     }
 }
