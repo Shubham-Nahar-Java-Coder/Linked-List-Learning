df375ab06404efcd4f022434c6a43001b6205e7a
Shubham Nahar <shunahar@publicisgroupe.net>
2024-07-08 11:14:21 +0530

    FindMiddleNode

---
+++ Interview_Questions/FindMiddleNode/Main.java
@@ -0,0 +1,18 @@
+package Interview_Questions;
+
+public class Main{
+    public static void main(String[] args) {
+        FindMiddleNode myList = new FindMiddleNode(1);
+
+        myList.append(2);
+        myList.append(3);
+        myList.append(4);
+        myList.append(5);
+        
+        System.out.println(myList.findMiddleNode().value);
+
+        myList.append(6);
+
+        System.out.println("After Appending One More value: "+ myList.findMiddleNode().value);
+    }
+}
