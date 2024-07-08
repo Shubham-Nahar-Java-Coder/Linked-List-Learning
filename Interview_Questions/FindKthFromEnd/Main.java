a9fcfc1b6a7b7abe1460a919a99f01ef22cb1372
Shubham Nahar <shunahar@publicisgroupe.net>
2024-07-08 13:21:18 +0530

    Find Kth From End

---
+++ Interview_Questions/FindKthFromEnd/Main.java
@@ -0,0 +1,20 @@
+package Interview_Questions.FindKthFromEnd;
+
+public class Main {
+
+    public static void main(String[] args) {
+ 
+        FindKthFromEnd myList = new FindKthFromEnd(1);
+
+        myList.append(2);
+        myList.append(3);
+        myList.append(4);
+        myList.append(5);
+
+        myList.printList();
+
+        System.out.println("2nd Kth Element from end " +myList.findKthFromEnd(2).value);
+
+        System.out.println("4th Kth Element from End: "+myList.findKthFromEnd(4).value);
+    }   
+}
